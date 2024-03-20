938290934a5cac1b97018b67b0e5ac48fddd102a
coolworld2049 <coolworld2049@gmail.com>
2024-03-20 17:56:01 +0300

    add jupyter lab

---
+++ benchmark/scripts/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+LOG_DIR=${LOG_DIR:-./logs/}
+
+rm -rf "$LOG_DIR"
+
+mkdir -p "$LOG_DIR"
+
+dotnet run ShopClient.dll --config nginx_config.prod.json --log "$LOG_DIR"/load_test_"$(hostname -f)".csv
