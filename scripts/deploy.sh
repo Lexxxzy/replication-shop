84f9d28058232480e3640d939db3b0a99b0e1bbf
coolworld2049 <coolworld2049@gmail.com>
2024-03-20 10:09:05 +0300

    upd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,3 +11,7 @@ fi
 
 PGPOOL_BACKEND_NODES=1:pg-1:5432,2:pg-2:5432,3:${SERVER_IP}:5432,4:${SERVER_IP}:5433 \
 docker compose -f docker-compose.postgresql.yml up -d
+
+docker compose -f docker-compose.redis.yml up -d
+
+docker compose -f docker-compose.app.yml up -d
