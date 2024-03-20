956c8947bcf5757e5140b9e9f12c1167f0559950
coolworld2049 <coolworld2049@gmail.com>
2024-03-20 05:48:25 +0300

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-export REPMGR_PARTNER_NODES=pg-1,pg-2,ubuntu-server-1-pg-1,ubuntu-server-1-pg-2
-export PGPOOL_BACKEND_NODES=1:pg-1:5432,2:pg-2:5432,3:192.168.188.132:5432,4:192.168.188.132:5433
-export PGPOOL_BACKEND_APPLICATION_NAMES=pg-1,pg-2,ubuntu-server-1-pg-1,ubuntu-server-1-pg-2
+SERVER_IP=$1
+
+export REPMGR_PARTNER_NODES=pg-1,pg-2
+export PGPOOL_BACKEND_NODES=1:pg-1:5432,2:pg-2:5432,3:${SERVER_IP}:5432,4:${SERVER_IP}:5433
 
 docker compose -f docker-compose.postgresql.yml up -d
