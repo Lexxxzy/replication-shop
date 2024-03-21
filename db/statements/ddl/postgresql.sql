5b9c4c5beeb41b2cfff22cbaef3cfeb6de8f425e
coolworld2049 <coolworld2049@gmail.com>
2024-03-21 23:10:02 +0300

    [postgres] replace integer to serial

--- db/statements/ddl/postgresql.sql
+++ db/statements/ddl/postgresql.sql
@@ -1,7 +1,7 @@
 -- Create tables
 CREATE TABLE public.cart
 (
-    id       integer NOT NULL,
+    id       bigserial NOT NULL,
     user_id  uuid    NOT NULL,
     added_at timestamp with time zone DEFAULT now(),
     PRIMARY KEY (id)
@@ -26,7 +26,7 @@ CREATE TABLE public.order_items
 
 CREATE TABLE public.orders
 (
-    id               integer NOT NULL,
+    id               bigserial NOT NULL,
     user_id          uuid    NOT NULL,
     delivery_address character(256),
     order_date       timestamp with time zone DEFAULT now(),
@@ -43,7 +43,7 @@ CREATE TABLE public.product_types
 
 CREATE TABLE public.products
 (
-    id              integer        NOT NULL,
+    id              bigserial        NOT NULL,
     name            character(128) NOT NULL,
     price           numeric(10, 2) NOT NULL,
     manufacturer    character(64),
@@ -62,42 +62,3 @@ CREATE TABLE public.users
     PRIMARY KEY (id),
     UNIQUE (email)
 );
-
--- Create sequences
-CREATE SEQUENCE public.cart_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-CREATE SEQUENCE public.orders_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-CREATE SEQUENCE public.product_types_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-CREATE SEQUENCE public.products_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
--- Set default values for sequences
-SELECT pg_catalog.setval('public.cart_id_seq', 1, true);
-SELECT pg_catalog.setval('public.orders_id_seq', 8, true);
-SELECT pg_catalog.setval('public.product_types_id_seq', 4, true);
-SELECT pg_catalog.setval('public.products_id_seq', 43, true);
