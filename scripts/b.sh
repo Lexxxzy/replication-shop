8d69c183f7097d5a2b5f4aa35197c12792ae2a31
coolworld2049 <coolworld2049@gmail.com>
2024-03-23 20:53:56 +0300

    new benchmark run method

--- scripts/b.sh
+++ scripts/b.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -f ./benchmark/Dockerfile https://github.com/Lexxxzy/replication-shop.git#"${BRANCH:-dev}" -t replication-shop-benchmark:latest
+BRANCHES=("feature/cassandra-cluster" "feature/postgresql-cluster" "feature/redis")
+
+for branch_name in "${BRANCHES[@]}"; do
+    printf "Building image for branch: %s\n" "$branch_name"
+    docker build -f ./benchmark/Dockerfile "https://github.com/Lexxxzy/replication-shop.git#$branch_name"\
+     -t "replication-shop-benchmark-$branch_name:latest"
+done
